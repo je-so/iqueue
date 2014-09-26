0d9a38d3c44393cee936c0c17100f103bce9ad2b
Jörg Seebohn <joerg.seebohn@web.de>
2014-09-26 15:21:28 +0200

    Implement first concept of iqueue interface

--- include/iqueue.h
+++ include/iqueue.h
@@ -7,25 +7,71 @@
 
    Copyright:
    This program is free software. See accompanying LICENSE file.
-   
+
    Author:
    (C) 2014 Jörg Seebohn
 */
 #ifndef IQUEUE_HEADER
 #define IQUEUE_HEADER
 
-typedef struct iqueue_t {
-   int dummy;
+#include <pthread.h>
 
+typedef struct iqsignal_t {
+   pthread_mutex_t lock;
+   pthread_cond_t  cond;
+} iqsignal_t;
+
+typedef struct iqmsg_t {
+   iqsignal_t* signal;
+   int processed;
+} iqmsg_t;
+
+typedef struct iqueue_t {
+   size_t  size;
+   size_t  readpos;
+   size_t  writepos;
+   int     waitreader;
+   int     waitwriter;
+   int     closed;
+   pthread_mutex_t readlock;
+   pthread_mutex_t writelock;
+   pthread_cond_t  readcond;
+   pthread_cond_t  writecond;
+   void*   msg[/*size*/];
 } iqueue_t;
+
+// === iqueue_t ===
 
 // Initializes queue
 // Possible error codes: ENOMEM
-int init_iqueue(/*out*/iqueue_t * queue);
+int new_iqueue(/*out*/iqueue_t ** queue, size_t size);
 
-// Frees resources of an initialized queue
-int free_iqueue(iqueue_t * queue);
+// Frees all resources of queue. Close is called automatically.
+int delete_iqueue(iqueue_t ** queue);
 
+// Marks queue as closed and wakes up any waiting reader/writer.
+// Blocks until all read/writer has left queue.
+void close_iqueue(iqueue_t * queue);
+
+// Stores msg in queue. EAGAIN is returned if queue is full.
+// EPIPE is returned if queue is closed.
+int trysend_iqueue(iqueue_t * queue, iqmsg_t * msg);
+
+// Stores msg in queue. Blocks if queue is full.
+// EPIPE is returned if queue is closed.
+int send_iqueue(iqueue_t * queue, iqmsg_t * msg);
+
+// Receives msg from queue. EAGAIN is returned if queue is empty.
+// EPIPE is returned if queue is closed.
+int tryrecv_iqueue(iqueue_t * queue, /*out*/iqmsg_t ** msg);
+
+// Receives msg from queue. Blocks if queue is empty.
+// EPIPE is returned if queue is closed.
+int recv_iqueue(iqueue_t * queue, /*out*/iqmsg_t ** msg);
+
+// === iqmsg_t ===
+
+// === iqsignal_t ===
 
 
 
