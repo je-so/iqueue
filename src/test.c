7f579d1da7f4a86b8ae4c61322638a2499020d9d
Jörg Seebohn <joerg.seebohn@web.de>
2014-09-26 15:23:40 +0200

    Implement test_initfree

--- src/test.c
+++ src/test.c
@@ -1,4 +1,5 @@
 // #define _GNU_SOURCE
+#include "iqueue.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <malloc.h>
@@ -9,12 +10,17 @@
 
 #define TEST(COND) \
          if (!(COND)) { \
-            fprintf(stderr, "%s:%d: TEST failed\n", __FILE__, __LINE__); \
+            fprintf(stderr, "\n%s:%d: TEST failed\n", __FILE__, __LINE__); \
             exit(1); \
          }
 
+
+#define PASS() \
+         printf("."); \
+         fflush(stdout);
+
 #ifdef __linux
-/* 
+/*
  * Uses GNU malloc_stats extension.
  * This function returns internal collected statistics
  * about memory usage so implementing a thin wrapper
@@ -52,7 +58,7 @@ int allocated_bytes(/*out*/size_t * nrofbytes)
       perror("pipe");
       goto ONERR;
    }
-   
+
    int flags = fcntl(pfd[0], F_GETFL);
    fcntl(pfd[0], F_SETFL, flags | O_NONBLOCK);
 
@@ -148,17 +154,135 @@ int allocated_bytes(size_t * nrofbytes)
 
 #endif
 
+static void* thr_lock(void * param)
+{
+   iqueue_t* queue = param;
+
+   TEST(0 == pthread_mutex_lock(&queue->writelock));
+   __sync_val_compare_and_swap(&queue->closed, 0, 1);
+   TEST(0 == pthread_cond_wait(&queue->writecond, &queue->writelock));
+   TEST(0 == pthread_mutex_unlock(&queue->writelock));
+
+   TEST(0 == pthread_mutex_lock(&queue->readlock));
+   __sync_val_compare_and_swap(&queue->closed, 1, 2);
+   TEST(0 == pthread_cond_wait(&queue->readcond, &queue->readlock));
+   TEST(0 == pthread_mutex_unlock(&queue->readlock));
+   __sync_val_compare_and_swap(&queue->closed, 2, 3);
+
+   return 0;
+}
+
+static void test_initfree(void)
+{
+   pthread_t thr;
+   iqueue_t* queue = 0;
+
+   // TEST new_iqueue
+   TEST(0 == new_iqueue(&queue, 12345));
+   TEST(0 != queue);
+   TEST(12345 == queue->size);
+   TEST(0 == queue->readpos);
+   TEST(0 == queue->writepos);
+   TEST(0 == queue->waitreader);
+   TEST(0 == queue->waitwriter);
+   TEST(0 == queue->closed);
+   for (size_t i = 0; i < queue->size; ++i) {
+      TEST(0 == queue->msg[i]);
+   }
+   // test writelock + writecond
+   TEST(0 == pthread_create(&thr, 0, &thr_lock, queue));
+   for (int i = 0; i < 10000; ++i) {
+      if (0 != __sync_val_compare_and_swap(&queue->closed, 0, 0)) break;
+      sched_yield();
+   }
+   // thr_lock is waiting on writecond
+   TEST(1 == __sync_val_compare_and_swap(&queue->closed, 0, 0));
+   TEST(0 == pthread_mutex_lock(&queue->writelock));
+   TEST(0 == pthread_cond_signal(&queue->writecond));
+   for (int i = 0; i < 10; ++i) {
+      sched_yield();
+      TEST(1 == __sync_val_compare_and_swap(&queue->closed, 0, 0));
+   }
+   TEST(0 == pthread_mutex_unlock(&queue->writelock));
+   for (int i = 0; i < 10000; ++i) {
+      if (1 != __sync_val_compare_and_swap(&queue->closed, 0, 0)) break;
+      sched_yield();
+   }
+   // thr_lock is waiting on readcond
+   TEST(2 == __sync_val_compare_and_swap(&queue->closed, 0, 0));
+   TEST(0 == pthread_mutex_lock(&queue->readlock));
+   TEST(0 == pthread_cond_signal(&queue->readcond));
+   for (int i = 0; i < 10; ++i) {
+      sched_yield();
+      TEST(2 == __sync_val_compare_and_swap(&queue->closed, 0, 0));
+   }
+   TEST(0 == pthread_mutex_unlock(&queue->readlock));
+   TEST(0 == pthread_join(thr, 0));
+   TEST(3 == __sync_val_compare_and_swap(&queue->closed, 0, 0));
+   PASS();
+
+   // TEST delete_iqueue
+   TEST(0 == delete_iqueue(&queue));
+   TEST(0 == queue);
+   PASS();
+
+   // TEST new_iqueue: different size parameter
+   for (size_t s = 1; s <= 65536; s <<= 1, s += 33) {
+      TEST(0 == new_iqueue(&queue, s));
+      TEST(0 != queue);
+      TEST(s == queue->size);
+      TEST(0 == queue->readpos);
+      TEST(0 == queue->writepos);
+      TEST(0 == queue->waitreader);
+      TEST(0 == queue->waitwriter);
+      TEST(0 == queue->closed);
+      for (size_t i = 0; i < queue->size; ++i) {
+         TEST(0 == queue->msg[i]);
+      }
+      TEST(0 == delete_iqueue(&queue));
+      TEST(0 == queue);
+   }
+   PASS();
+
+   // TEST new_iqueue: EINVAL
+   TEST(EINVAL == new_iqueue(&queue, 0));
+   TEST(EINVAL == new_iqueue(&queue, ((size_t)-1 - sizeof(iqueue_t))/sizeof(void*)));
+   PASS();
+
+   // TEST new_iqueue: ENOMEM
+   TEST(ENOMEM == new_iqueue(&queue, (size_t)-1/(2*sizeof(void*))));
+   PASS();
+}
+
+static void test_sendrecv_single(void);
+{
+   // sending and receiving with single thread
+
+   // TODO: implement
+
+   PASS();
+}
+
 int main(void)
 {
    size_t nrofbytes;
    size_t nrofbytes2;
-   
-   printf("Running iqueue test\n");
 
-   TEST(0 == allocated_bytes(&nrofbytes));
+   printf("Running iqueue test: ");
+   fflush(stdout);
 
+   // repeat tests cause first call to pthread api allocates memory
+   for (int i = 0; i < 2; ++i) {
+      TEST(0 == allocated_bytes(&nrofbytes));
 
-   TEST(0 == allocated_bytes(&nrofbytes2));
+      test_initfree();
+      test_sendrecv_single();
+
+      TEST(0 == allocated_bytes(&nrofbytes2));
+      if (nrofbytes == nrofbytes2) break;
+   }
+
+   printf("\n");
 
    if (nrofbytes != nrofbytes2) {
       printf("Memory leak of '%ld' bytes!\n", (long) (nrofbytes2 - nrofbytes));
