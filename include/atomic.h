364dcc58a35d28f8df3c11fdf4335277aaa8c953
Jörg Seebohn <joerg.seebohn@web.de>
2014-09-27 18:27:33 +0200

    Implement and_atomicptr

--- include/atomic.h
+++ include/atomic.h
@@ -14,7 +14,7 @@
 // See https://gcc.gnu.org/onlinedocs/gcc-4.1.2/gcc/Atomic-Builtins.html for a description
 
 // Does the following operations in one atomic step:
-// { size_t old = *pval; if (old == oldval) *pval = newval; return old; }
+// { void* old = *pval; if (old == oldval) *pval = newval; return old; }
 static inline void* cmpxchg_atomicptr(void** pval, void* oldval, void* newval)
 {
          return __sync_val_compare_and_swap(pval, oldval, newval);
@@ -40,10 +40,18 @@ static inline size_t add_atomicsize(size_t* pval, size_t value)
 }
 
 // Does the following operations in one atomic step:
+// { void* old = *pval; *pval &= value; return old; }
+static inline void* and_atomicptr(void** pval, void* value)
+{
+         return __sync_fetch_and_and(pval, value);
+}
+
+// Does the following operations in one atomic step:
 // { size_t old = *pval; *pval -= value; return old; }
 static inline size_t sub_atomicsize(size_t* pval, size_t value)
 {
          return __sync_fetch_and_sub(pval, value);
 }
+
 
 #endif
